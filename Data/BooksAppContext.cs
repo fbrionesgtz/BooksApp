2e0974c9276d3b5c2f201900063311430fcca4f7
Francisco Briones <f.brionesgtz@gmail.com>
2022-07-11 14:47:20 -0400

    Initial setup, book class, database, and CRUD implemented

---
+++ Data/BooksAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BooksApp.Models;
+
+namespace BooksApp.Data
+{
+    public class BooksAppContext : DbContext
+    {
+        public BooksAppContext (DbContextOptions<BooksAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BooksApp.Models.Book> Book { get; set; }
+    }
+}
---
+++ Models/Books.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BooksApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int YearPublished { get; set; }
+
+        public Book()
+        {
+
+        }
+    }
+}
